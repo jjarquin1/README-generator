6a1c727df506d8c04ded78ce9ea88e014b8f526d
jkooda <77827825+jjarquin1@users.noreply.github.com>
2021-05-19 19:06:02 -0700

    removed unused code

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,16 +1,4 @@
 
-// function getLicense(license) {
-//   let badge = "";
-//   if (data.license == 'Mit License') {
-//     badge = [![License-MIT]('https://img.shields.io/badge/License-MIT-yellow.svg')]('https://opensource.org/licenses/MIT')
-//   } else if (data.license == 'IBM License') {
-//     badge = [![License-APC]('https://img.shields.io/badge/License-Apache%202.0-blue.svg')]('https://opensource.org/licenses/Apache-2.0')
-//   } else if (data.license == 'Apache License') {
-//     badge = [![License-IBM]('https://img.shields.io/badge/License-IPL%201.0-blue.svg')]('https://opensource.org/licenses/IPL-1.0')
-//   }
-//   return badge; 
-// }
-
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `## ${data.title}
